cff18fd7abffc37b4b05d2233fb79ccf18ebb794
phramusca <raphael.camus@free.fr>
2022-12-04 22:28:49 +0100

    db in internal sd

---
+++ app/src/main/java/org/phramusca/cookandfreeze/helpers/HelperToast.java
@@ -0,0 +1,34 @@
+package org.phramusca.cookandfreeze.helpers;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by raph on 10/06/17.
+ */
+
+public class HelperToast {
+
+    private static final String TAG = HelperToast.class.getName();
+    private final Context context;
+
+    public HelperToast(Context context) {
+        this.context = context;
+    }
+
+    public void toastLong(final String msg) {
+        toast(msg, Toast.LENGTH_LONG);
+    }
+
+    public void toastShort(final String msg) {
+        toast(msg, Toast.LENGTH_SHORT);
+    }
+
+    private void toast(final String msg, int duration) {
+        if (!msg.equals("")) {
+            Log.i(TAG, "Toast makeText " + msg); //NON-NLS
+            Toast.makeText(context, msg, duration).show();
+        }
+    }
+}
